7aecd1bd3c4cadd4b402abfe9a3e7e2ed6249784
ASUS <ASUS@DESKTOP-93HN35M>
2022-08-01 18:45:59 +0530

    ByPawan

---
+++ Git_1August/src/August1_Task/Program1.java
@@ -0,0 +1,9 @@
+package August1_Task;
+
+public class Program1 
+{
+		public static void main(String[] args) 
+		{
+			System.out.println("Program1");
+		}
+}
--- Git_1August/src/Git_1August/Demo.java
+++ Git_1August/src/Git_1August/Demo.java
@@ -4,7 +4,7 @@ public class Demo
 {		
 		public void m1()
 		{
-			System.out.println("Add----m1")
+			System.out.println("Add----m1");
 		}
 	
 		public static void main(String[] args) 
