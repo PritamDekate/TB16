e9e09f90f496ee3f46f3d1dc8a3b0abacd91f956
PritamDekate <110393155+PritamDekate@users.noreply.github.com>
2022-08-01 18:23:22 +0530

    Update Demo.java
    By Pritam

--- Git_1August/src/Git_1August/Demo.java
+++ Git_1August/src/Git_1August/Demo.java
@@ -1,7 +1,12 @@
 package Git_1August;
 
 public class Demo 
-{
+{		
+		public void m1()
+		{
+			System.out.println("Add----m1")
+		}
+	
 		public static void main(String[] args) 
 		{
 			System.out.println("Git");
