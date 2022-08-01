519e415c6f9a45621797ac387c7e08e94d60407e
PritamDekate <110393155+PritamDekate@users.noreply.github.com>
2022-08-01 18:49:19 +0530

    Update Program1.java

--- Git_1August/src/August1_Task/Program1.java
+++ Git_1August/src/August1_Task/Program1.java
@@ -1,7 +1,17 @@
 package August1_Task;
 
 public class Program1 
-{
+{		
+		public void m1()
+		{
+			System.out.println("m1-----");
+		}	
+	
+		public void m2()
+		{
+			System.out.println("m2-----");
+		}
+	
 		public static void main(String[] args) 
 		{
 			System.out.println("Program1");
